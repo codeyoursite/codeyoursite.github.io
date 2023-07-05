28ec7cc14f6eb2bf810e6ecee75248e206efe590
theguythatloveshtml <126412862+theguythatloveshtml@users.noreply.github.com>
2023-07-05 15:25:22 +0100

    Update main.js

--- main.js
+++ main.js
@@ -10,9 +10,10 @@ function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("myDiv").style.display = "block";
   const images = [
-  "https://images.unsplash.com/photo-1615568929336-ddfd93ce683a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1526&q=80",
-  "https://images.unsplash.com/photo-1588250318414-908688966feb?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=2089&q=80",
-  "https://images.unsplash.com/photo-1524077410193-2f667f9ab09a?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1650&q=80"
+  "developer.jpg",
+  "thumbnail_IMG_9690.jpg",
+  "thumbnail_image1.jpg"
+  "thumbnail_image0.jpg"
   ];
   let i = 0;
   changePicture()
