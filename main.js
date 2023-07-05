a2c7a59bf57b1aa18a42f8a670e099f69c258435
theguythatloveshtml <126412862+theguythatloveshtml@users.noreply.github.com>
2023-07-05 12:10:13 +0100

    Update main.js

--- main.js
+++ main.js
@@ -8,24 +8,3 @@ function showPage() {
   document.getElementById("loader").style.display = "none";
   document.getElementById("myDiv").style.display = "block";
 }
-var state = 0
-function changeState(newState)
-{
-  switch(newState)
-  {
-    case 0:
-      break;
-    case 1:
-      const page2 = document.getElementById("page2");
-      page2.scrollIntoView();
-      break;
-    case 2:
-      const page3 = document.getElementById("page3");
-      page3.scrollIntoView();
-  }
-}
-document.addEventListener("click", function()
-{
-  state += 1;
-  changeState(state);
-});
