eae8c53e1e50e944a321b2b1de8447c801b10e69
codeyoursite <142327539+codeyoursite@users.noreply.github.com>
2023-09-10 15:07:35 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const toggleBtnIcon = document.querySelector('.bars i');
 const dropDownMenu = document.querySelector('.dropdown_menu');
 const oneBtn = document.querySelector('.dropdown_link_one');
 const twoBtn = document.querySelector('.dropdown_link_two');
-const oneBtnIcon = document.querySelector('.one_icon');
+const oneBtnIcon = document.querySelector('.dropdown_link_one i');
 const twoBtnIcon = document.querySelector('.two_icon');
 const oneMenu = document.querySelector('.dropdown_content_one');
 const twoMenu = document.querySelector('.dropdown_content_two');
@@ -14,13 +14,16 @@ toggleBtn.onclick = function () {
   var li = document.getElementById("li");
   const isOpen = dropDownMenu.classList.contains('open');
   toggleBtnIcon.classList = isOpen
+    ? 'fa-solid fa-caret-up'
+    : 'fa-solid fa-caret-down'
+}
+
+oneBtn.onclick = function () {
+  oneMenu.classList.toggle('open');
+  const isOpen = oneMenu.classList.contains('open');
+  oneBtnIcon.classList = isOpen
     ? 'fa-solid fa-xmark'
     : 'fa-solid fa-bars'
-  if (li.style.display === "block") {
-    li.style.display = "none";
-  } else {
-    li.style.display = "block";
-  }
 }
 
 function myFunction() {
