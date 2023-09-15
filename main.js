e8201b12dc2df07f30ea44036e324f7fdf8a12a6
codeyoursite <142327539+codeyoursite@users.noreply.github.com>
2023-09-15 14:59:09 +0100

    Update main.js

--- main.js
+++ main.js
@@ -76,6 +76,7 @@ const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+start_canvas()
 
 document.addEventListener("resize", resize);
 
