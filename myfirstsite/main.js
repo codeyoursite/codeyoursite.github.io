4c2297af18bd3efab5b1fb6a9eaad49fd9ce9d5c
codeyoursite <142327539+codeyoursite@users.noreply.github.com>
2023-09-29 15:13:30 +0100

    Commit

--- myfirstsite/main.js
+++ myfirstsite/main.js
@@ -1,5 +1,7 @@
 const start = document.getElementsById('start');
+const main = document.getElementsById('main');
 
 window.addEventListener("click", function(){
-    start.classList.add("hidden");
+    start.style.display = "none";
+    main.style.display = "block";
 });
