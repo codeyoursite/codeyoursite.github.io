eb5bc9c24b0a681469815b68697b2f58900a9488
codeyoursite <142327539+codeyoursite@users.noreply.github.com>
2023-10-03 15:46:58 +0100

    Typewriter

--- main.js
+++ main.js
@@ -69,27 +69,59 @@ function showPage() {
   typeWriter();
 }
 
+var TxtType = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
 
-var i = 0;
-var texts = ['Thanks for checking out my site.', 'I can code your website.', 'I build with pure code. "Your site will be very easy to customise.'];
-var txt = 'Thanks for checking out my site.';
+TxtType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
 
-// produces the typing effect on welcome message when page is loaded
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("heading").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, 70);
-  } 
-  else {
-    deleteWriter();
+  if (this.isDeleting) {
+  this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+  this.txt = fullTxt.substring(0, this.txt.length + 1);
   }
-}
 
-function deleteWriter() {
-  if (txt.length > 0) {
-    var currentText = document.getElementById("heading").innerHTML
-    document.getElementById("heading").innerHTML = currentText.slice(0, currentText.length - 1);
-    setTimeout(deleteWriter, 70);
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+  delta = this.period;
+  this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+  this.isDeleting = false;
+  this.loopNum++;
+  delta = 500;
   }
-}
+
+  setTimeout(function() {
+  that.tick();
+  }, delta);
+};
+
+window.onload = function() {
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-type');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtType(elements[i], JSON.parse(toRotate), period);
+      }
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
+};
