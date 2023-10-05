0daf16dbdb86e51711f5581ec55e39995d162ee3
codeyoursite <142327539+codeyoursite@users.noreply.github.com>
2023-10-05 18:55:59 +0100

    Dark/Light Modes

--- main.js
+++ main.js
@@ -120,3 +120,43 @@ window.onload = function() {
   css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #000}";
   document.body.appendChild(css);
 };
+
+function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
+  if (localStorageTheme !== null) {
+    return localStorageTheme;
+  }
+
+  if (systemSettingDark.matches) {
+    return "dark";
+  }
+
+  return "light";
+}
+
+const localStorageTheme = localStorage.getItem("theme");
+const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
+
+let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingDark });
+
+const button = document.querySelector("[data-theme-toggle]");
+
+button.addEventListener("click", () => {
+  const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
+
+  // update the button text
+  const newCta = newTheme === "dark" ? "Change to light theme?" : "Change to dark theme?";
+  button.innerText = newCta;  
+
+  // use an aria-label if you are omitting text on the button
+  // and using sun/moon icons, for example
+  button.setAttribute("aria-label", newCta);
+
+  // update theme attribute on HTML to switch theme in CSS
+  document.querySelector("html").setAttribute("data-theme", newTheme);
+
+  // update in local storage
+  localStorage.setItem("theme", newTheme);
+
+  // update the currentThemeSetting in memory
+  currentThemeSetting = newTheme;
+});
