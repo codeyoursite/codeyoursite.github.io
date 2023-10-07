af0b2ee3a004a19feaa3c97cc16ed1af5cfddfe2
codeyoursite <142327539+codeyoursite@users.noreply.github.com>
2023-10-07 09:51:17 +0100

    Update main.js

--- main.js
+++ main.js
@@ -117,7 +117,7 @@ window.onload = function() {
   // INJECT CSS
   var css = document.createElement("style");
   css.type = "text/css";
-  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #000}";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid var(--typewriter-color)}";
   document.body.appendChild(css);
 };
 
