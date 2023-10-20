c2e7d0f698c8daea53f338ab795a836db078e008
codeyoursite <142327539+codeyoursite@users.noreply.github.com>
2023-10-20 14:20:16 +0100

    padding change

--- myfirstsite/main.js
+++ myfirstsite/main.js
@@ -3,9 +3,5 @@ const main = document.getElementById('main');
 
 window.addEventListener("click", function(){
     start.style.opacity = "0";
-    setTimeout(displayBlock, 400);
-});
-
-function displayBlock() {
     main.style.display = "block";
 }
