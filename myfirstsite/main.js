a559685f568d12c6e37b632ac90a1fed3cd05fc1
codeyoursite <142327539+codeyoursite@users.noreply.github.com>
2023-10-20 14:22:05 +0100

    Margin

--- myfirstsite/main.js
+++ myfirstsite/main.js
@@ -1,7 +1,7 @@
 const start = document.getElementById('start');
 const main = document.getElementById('main');
 
-window.addEventListener("click", function(){
+window.addEventListener("click", function() {
     start.style.opacity = "0";
     main.style.display = "block";
-}
+});
