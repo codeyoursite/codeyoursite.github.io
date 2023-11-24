d3085305748a99ef3d0f602b4e45fca279b469b9
codeyoursite <142327539+codeyoursite@users.noreply.github.com>
2023-11-24 13:36:12 +0000

    Update main.js

--- myfirstsite/main.js
+++ myfirstsite/main.js
@@ -2,6 +2,6 @@ const start = document.getElementById('start');
 const main = document.getElementById('main');
 
 window.addEventListener("click", function() {
-    document.start.classList.add('start-hidden');
+    start.classList.add('start-hidden');
     main.style.display = "block";
 });
