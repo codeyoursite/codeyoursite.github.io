16dda3f347e7f7e7a00d98e4c9047d7c4e94c42c
codeyoursite <142327539+codeyoursite@users.noreply.github.com>
2024-01-23 11:42:36 +0000

    Update main.js

--- digitalleaders/website1/main.js
+++ digitalleaders/website1/main.js
@@ -3,13 +3,16 @@ const two = document.querySelector('.two');
 const btn = document.querySelector('.btn');
 
 function click(){
+  console.log("The 'click' function has started.");
   if (two.style.display = "none") {
     two.style.display = "block";
     one.style.display = "none";
     btn.innerHTML = '← Previous';
+    console.log("One is now hidden.");
   } else {
     one.style.display = "block";
     two.style.display = "none";
     btn.innerHTML = 'Next →';
+    console.log("Two is now hidden.");
   }
 };
