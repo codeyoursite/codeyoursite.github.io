6ce0d97e0060cda35f59b4d8949b501bba274dc3
codeyoursite <142327539+codeyoursite@users.noreply.github.com>
2024-01-23 13:09:43 +0000

    Update main.js

--- digitalleaders/website1/main.js
+++ digitalleaders/website1/main.js
@@ -2,19 +2,22 @@ const one = document.querySelector('.one');
 const two = document.querySelector('.two');
 const btn = document.querySelector('.btn');
 
-btn.onclick = function() {click()};
+btn.onclick = function() {
+  click();
+};
 
 function click() {
   console.log("The 'click' function has started.");
-  if (two.style.display = "none") {
+
+  if (two.style.display === "none") {
     two.style.display = "block";
     one.style.display = "none";
     btn.innerHTML = '← Previous';
     console.log("One is now hidden.");
-  } else if (one.style.display = "none") {
+  } else if (one.style.display === "none") {
     one.style.display = "block";
     two.style.display = "none";
     btn.innerHTML = 'Next →';
     console.log("Two is now hidden.");
   }
-};
+}
