74b283ffaa8fd1cb2ce7769083a37648b3894246
codeyoursite <142327539+codeyoursite@users.noreply.github.com>
2024-02-22 18:07:07 +0000

    Update main.js

--- fun/firstgame/game/main.js
+++ fun/firstgame/game/main.js
@@ -1,6 +1,7 @@
 const button = document.getElementById("btn");
+const txt = document.getElementById("text");
 let clicks = 0;
-let complete = 1;
+let complete = 0;
 
 button.addEventListener('click', function(e){
     e.preventDefault();
@@ -12,7 +13,28 @@ button.addEventListener('click', function(e){
     }
 });
 
-const Timeout = setTimeout(end, 10000);
+const beginOne = setTimeout(one, 1000);
+const beginTwo = setTimeout(two, 2000);
+const beginThree = setTimeout(three, 3000);
+const beginFour = setTimeout(begin, 4000);
+
+function one() {
+    txt.innerText = "3...";
+}
+
+function two() {
+    txt.innerText = "2...";
+}
+
+function three() {
+    txt.innerText = "1...";
+}
+
+function begin() {
+    txt.innerText = "Click the button! Your timer has started.";
+    complete++;
+    const end = setTimeout(end, 10000);
+}
 
 function end() {
     console.log("Game Ended");
