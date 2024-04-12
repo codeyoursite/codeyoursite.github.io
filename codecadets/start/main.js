79dab5e59cf90158d1911e13d5f28a6c6ba0770b
codeyoursite <142327539+codeyoursite@users.noreply.github.com>
2024-04-12 12:10:41 +0100

    Add files via upload

---
+++ codecadets/start/main.js
@@ -0,0 +1,16 @@
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const age = document.getElementById("floatingPassword").value;
+    let data = new URLSearchParams();
+    data.append("age", age);
+    if (age < 7) {
+        console.log("P2")
+        const url = `../p2`;
+        window.location.href = url;
+    } else {
+        const url = `../second/index.html?${data.toString()}`;
+        window.location.href = url;
+    }
+});
