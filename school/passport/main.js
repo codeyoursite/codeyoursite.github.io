5954650ef6696c13c4773a49ec148924195bf071
codeyoursite <142327539+codeyoursite@users.noreply.github.com>
2024-04-18 13:29:13 +0100

    [Insert Commit Name Here]

---
+++ school/passport/main.js
@@ -0,0 +1,6 @@
+document.body.addEventListener(showPage, 1500) 
+  
+function showPage() {
+    document.getElementById("start").style.display = "none";
+    document.getElementById("end").style.display = "block";
+}
