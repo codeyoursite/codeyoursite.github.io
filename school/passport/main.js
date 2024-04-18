7546676c2891e9844aa7bb19179a81f5e9acd8ed
codeyoursite <142327539+codeyoursite@users.noreply.github.com>
2024-04-18 13:33:23 +0100

    Finishing Loader

--- school/passport/main.js
+++ school/passport/main.js
@@ -1,6 +1,8 @@
-document.body.addEventListener(showPage, 1500) 
+document.body.onload = function(){
+    document.body.addEventListener(showPage, 1200);
+}
   
 function showPage() {
     document.getElementById("start").style.display = "none";
-    document.getElementById("end").style.display = "block";
+    document.getElementById("page").style.display = "block";
 }
