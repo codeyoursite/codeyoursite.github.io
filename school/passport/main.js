48c593a41ed6b2275abd0098a71d9d48c6ea3033
codeyoursite <142327539+codeyoursite@users.noreply.github.com>
2024-04-19 17:31:08 +0100

    Update main.js

--- school/passport/main.js
+++ school/passport/main.js
@@ -6,3 +6,14 @@ function showPage() {
     document.getElementById("start").style.display = "none";
     document.getElementById("page").style.display = "block";
 }
+
+$( function() {
+		$( ".img" ).mgGlitch({
+	      destroy : false,
+          glitch: true,
+          scale: true,
+          blend : true,
+          blendModeType : 'hue',
+          glitch1TimeMin : 10
+		});
+});
