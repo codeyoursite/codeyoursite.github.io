29f00c448bcb276f7200e3606f4866eef88c68d2
codeyoursite <142327539+codeyoursite@users.noreply.github.com>
2024-04-19 19:24:22 +0100

    Update main.js

--- school/passport/main.js
+++ school/passport/main.js
@@ -1,9 +1,22 @@
 document.body.onload = function(){
-    setTimeout(showPage, 5000);
+    setTimeout(showPage, 4000);
     PowerGlitch.glitch('.img')
 }
   
 function showPage() {
-    document.getElementById("start").style.display = "none";
+    PowerGlitch.glitch('.start');
+    setTimeout(showPageTwo, 1500);
+}
+
+function showPageTwo() {
     document.getElementById("page").style.display = "block";
+    var el = document.getElementById(start);
+    let opacity = undefined;
+    setInterval(function() {
+      opacity = el.style.opacity;
+      if (opacity > 0) {
+         opacity -= 0.01;
+         el.style.opacity = opacity;
+      }
+    }, 8);
 }
