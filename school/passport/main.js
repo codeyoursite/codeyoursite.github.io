0640d1d398f0acccef217342f1811fc5e6656722
codeyoursite <142327539+codeyoursite@users.noreply.github.com>
2024-04-21 18:05:37 +0100

    Update main.js

--- school/passport/main.js
+++ school/passport/main.js
@@ -1,45 +1,44 @@
 document.body.onload = function(){
-    setTimeout(showPage, 4000);
     const power = PowerGlitch.glitch('.img');
+    setTimeout(function() {
+        showPage(power);
+    }, 4000);
 }
   
-function showPage() {
+function showPage(power) {
     const power2 = PowerGlitch.glitch('.start');
-    setTimeout(showPageTwo, 1500);
+    setTimeout(function() {
+        showPageTwo(power, power2)
+    }, 1500);
     setTimeout(showPageThree, 700);
 }
 
-function showPageTwo() {
+function showPageTwo(power, power2) {
     const el = document.getElementById("start");
     let opacity = 1;
     let times = 0;
     var interval = setInterval(function() {
-        repeat(interval, el);
+        repeat(interval, el, power, power2);
     }, 8);
-    console.log(`Finished opacity loop with ${times} tries.`);
-}
-
-function reload() {
-    location.reload();
 }
 
 function showPageThree() {
     document.getElementById("page").style.display = "block";
 }
 
-function repeat(interval, el) {
+function repeat(interval, el, power, power2) {
         let opacity = 1;
-        repeat2(opacity, interval, el);
+        repeat2(opacity, interval, el, power, power2);
 }
 
-function repeat2(opacity, interval, el) {
+function repeat2(opacity, interval, el, power, power2) {
     if (opacity > 0) {
             opacity -= 0.01;
             el.style.opacity = opacity;
-        } else {
+    } else {
             power.stopGlitch();
             power2.stopGlitch();
             el.style.display = "none";
             clearInterval(interval);
-        }
+    }
 }
