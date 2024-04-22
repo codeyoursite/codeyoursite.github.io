68930500ca2b87db1c8a5d487f1aa1c6a37f06bf
codeyoursite <142327539+codeyoursite@users.noreply.github.com>
2024-04-22 12:54:52 +0100

    Update main.js

--- school/passport/main.js
+++ school/passport/main.js
@@ -3,6 +3,9 @@ document.body.onload = function(){
 }
 
 function showPage() {
+    const img = document.getElementById("img");
+    console.log('Image Height = ' + img.clientHeight);
+    console.log('Image Width = ' + img.clientWidth);
     const start = document.getElementById("start");
     const page = document.getElementById("page");
     start.style.opacity = "0";
