cbf008d078eb15dfebc06d7ca436ae0441d6b5b1
codeyoursite <142327539+codeyoursite@users.noreply.github.com>
2024-04-25 14:12:44 +0100

    Commit new list

--- school/passport/main.js
+++ school/passport/main.js
@@ -4,8 +4,6 @@ document.body.onload = function(){
 
 function showPage() {
     const img = document.getElementById("img");
-    console.log('Image Height = ' + img.clientHeight);
-    console.log('Image Width = ' + img.clientWidth);
     const start = document.getElementById("start");
     const page = document.getElementById("page");
     start.style.opacity = "0";
