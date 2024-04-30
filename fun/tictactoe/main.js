fe494b598778be03c6302ffe75cdf8aa1e1f38db
Code Your Site <142327539+codeyoursite@users.noreply.github.com>
2024-04-30 16:29:33 +0100

    push

--- fun/tictactoe/main.js
+++ fun/tictactoe/main.js
@@ -7,3 +7,50 @@ const six = document.getElementById("six");
 const seven = document.getElementById("seven");
 const eight = document.getElementById("eight");
 const nine = document.getElementById("nine");
+
+let turn = undefined;
+let ones = undefined;
+let twos = undefined;
+let threes = undefined;
+let fours = undefined;
+let fives = undefined;
+let sixs = undefined;
+let sevens = undefined;
+let eights = undefined;
+let nines = undefined;
+
+one.addEventListener("click", function() {
+    one.style.opacity = "100%";
+});
+
+two.addEventListener("click", function() {
+    two.style.opacity = "100%";
+});
+
+three.addEventListener("click", function() {
+    three.style.opacity = "100%";
+});
+
+four.addEventListener("click", function() {
+    four.style.opacity = "100%";
+});
+
+five.addEventListener("click", function() {
+    five.style.opacity = "100%";
+});
+
+six.addEventListener("click", function() {
+    six.style.opacity = "100%";
+});
+
+seven.addEventListener("click", function() {
+    seven.style.opacity = "100%";
+});
+
+eight.addEventListener("click", function() {
+    eight.style.opacity = "100%";
+});
+
+nine.addEventListener("click", function() {
+    nine.style.opacity = "100%";
+});
