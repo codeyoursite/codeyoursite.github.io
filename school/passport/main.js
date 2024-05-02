443671727736ebf6a91cc581966e00b059960de7
Code Your Site <142327539+codeyoursite@users.noreply.github.com>
2024-05-02 18:17:44 +0100

    Oops

--- school/passport/main.js
+++ school/passport/main.js
@@ -9,7 +9,7 @@ function showPage() {
     start.style.opacity = "0";
     start.addEventListener('transitionend', () => start.remove());
     page.style.display = "block";
-    page.addEventListener(animationend, () => {movePage("first")})
+    page.addEventListener("animationend", () => {movePage("first")})
 }
 
 function movePage(id) {
