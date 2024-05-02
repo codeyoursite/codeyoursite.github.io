d5ce2082786df8e57c9c57bf2e9513279b483a55
Code Your Site <142327539+codeyoursite@users.noreply.github.com>
2024-05-02 19:25:44 +0100

    Fix

--- school/passport/main.js
+++ school/passport/main.js
@@ -1,50 +1,52 @@
-document.body.onload = function(){
-    setTimeout(showPage, 3600);
-}
+document.addEventListener('DOMContentLoaded', function() {
+  setTimeout(showPage, 3600);
+});
 
 function showPage() {
-    const img = document.getElementById("img");
-    const start = document.getElementById("start");
-    const page = document.getElementById("page");
-    start.style.opacity = "0";
-    start.addEventListener('transitionend', () => start.remove());
-    page.style.display = "block";
+  const img = document.getElementById("img");
+  const start = document.getElementById("start");
+  const page = document.getElementById("page");
+  start.style.opacity = "0";
+  start.addEventListener('transitionend', function() {
+      start.remove();
+  });
+  page.style.display = "block";
 }
 
 function movePage(id) {
-    const el = document.getElementById(id);
-    document.body.classList.remove("stop-scrolling");
-    el.scrollIntoView({ behavior: "smooth", });
-    document.body.classList.add("stop-scrolling");
+  const el = document.getElementById(id);
+  document.body.classList.remove("stop-scrolling");
+  el.scrollIntoView({ behavior: "smooth" });
+  document.body.classList.add("stop-scrolling");
 }
 
 let mybutton = document.getElementById("myBtn");
 let myhelp = document.getElementById("help");
 let seconds = 0;
 mybutton.style.display = "none";
-window.onscroll = function() {scrollFunction()};
+window.addEventListener('scroll', scrollFunction);
+
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    mybutton.style.display = "block";
-    myhelp.style.display = "none";
-    checkHelp();
+      mybutton.style.display = "block";
+      myhelp.style.display = "none";
+      checkHelp();
   } else {
-    mybutton.style.display = "none";
-    myhelp.style.display = "none";
+      mybutton.style.display = "none";
+      myhelp.style.display = "none";
   }
 }
 
 function checkHelp() {
-  while (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    if (seconds < 5) {
-      setTimeout(1000, changeTimer);
+  if (seconds < 5) {
+      setTimeout(changeTimer, 1000);
       myhelp.style.display = "none";
-    } else {
+  } else {
       myhelp.style.display = "block";
-    }
   }
 }
 
 function changeTimer() {
   seconds += 1;
+  checkHelp();
 }
