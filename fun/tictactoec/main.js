50a24a3dcdd6c3e9a0d0270c0ae4d0dec3f031e5
Code Your Site <142327539+codeyoursite@users.noreply.github.com>
2024-05-17 12:43:52 +0100

    Update main.js

--- fun/tictactoec/main.js
+++ fun/tictactoec/main.js
@@ -104,14 +104,17 @@ function computer() {
             a.textContent = turn;
             a.style.opacity = "100%";
             madeMove = true;
+            break;
         } else if (a.textContent == "X" && c.textContent == "X" && b.textContent == placeholder) {
             a.textContent = turn;
             a.style.opacity = "100%";
             madeMove = true;
+            break;
         } else if (b.textContent == "X" && c.textContent == "X" && a.textContent == placeholder) {
             a.textContent = turn;
             a.style.opacity = "100%";
             madeMove = true;
+            break;
         }
     }
 
