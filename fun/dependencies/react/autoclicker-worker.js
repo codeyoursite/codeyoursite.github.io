947db30a0a1404c223d0bf37b7f2b5c482b45ebe
Code Your Site <142327539+codeyoursite@users.noreply.github.com>
2025-09-11 13:52:51 +0100

    Update autoclicker-worker.js

--- fun/dependencies/react/autoclicker-worker.js
+++ fun/dependencies/react/autoclicker-worker.js
@@ -2,11 +2,14 @@ let timerId;
 let cursorCount = 0;
 
 self.addEventListener('message', function(event) {
-    if (event.data && event.data.type === 'updateCps') {
+    if (event.data && event.data.type === 'updateCps' && event.data.jims == 0) {
         const totalCps = (event.data.bumwackers * 1) + (event.data.wilsons * 5)  + (event.data.nippys * 20)  + (event.data.jims * 500); // 1 CPS for bumwackers, 5 for wilsons
-        console.log(totalCps)
         console.log('Worker: Received updateCps message. New total CPS:', totalCps);
         resetInterval(totalCps);
+    } else if (event.data.jims < 0) {
+        const totalCps = 500;
+        console.log('Worker: Received updateCps message. New total CPS:', totalCps);
+        bigInterval(totalCps);
     }
 });
 
@@ -26,6 +29,24 @@ function resetInterval(newCps) {
         console.log('Worker: Total CPS is zero, stopping interval.');
     }
 }
+
+function bigInterval(newCps) {
+    if (timerId) {
+        clearInterval(timerId);
+    }
+
+    if (newCps > 0) {
+        const delay = 1000; 
+        timerId = setInterval(() => {
+            self.postMessage('superclick');
+        }, delay);
+        console.log('Worker: Interval started with delay:', delay);
+    } else {
+        timerId = null;
+        console.log('Worker: Total CPS is zero, stopping interval.');
+    }
+}
+
 
 
 
